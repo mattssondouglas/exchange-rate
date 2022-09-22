fab896e9776741a199e2b8af85f0df52041d76d7
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-09-22 18:02:50 +0200

    add basic logic for rate

---
+++ script.js
@@ -0,0 +1,31 @@
+// Getting variables from document
+const currencyElementOne = document.getElementById("currency-one");
+const amountElementOne = document.getElementById("amount-one");
+const currencyElementTwo = document.getElementById("currency-two");
+const amountElementTwo = document.getElementById("amount-two");
+
+const rateElement = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+// Fetch exchange rates + updates DOM
+const calculate = () => {
+  const currencyOne = currencyElementOne.value;
+  const currencyTwo = currencyElementTwo.value;
+  fetch(
+    `https://v6.exchangerate-api.com/v6/77d443bbd8f0aa21d281d1d2/latest/${currencyOne}`
+  )
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.conversion_rates[currencyTwo];
+      rateElement.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+      amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
+    });
+};
+
+// Event listeners
+currencyElementOne.addEventListener("change", calculate);
+amountElementOne.addEventListener("input", calculate);
+currencyElementTwo.addEventListener("change", calculate);
+amountElementTwo.addEventListener("input", calculate);
+
+calculate();
